26417a01771d1f125f7e314feb4f6ae0f680d3c0
Mohammadhosein Hamedtavasoli <mhoseinhamed@gmail.com>
2018-11-17 17:58:03 +0100

    new_exp_10_mun

--- Data/Analysis.R
+++ Data/Analysis.R
@@ -1,6 +1,14 @@
 library(reshape2)
 library(ggplot2)
 library(plotly)
+# library(sdtoolkit)
+# library(prim)
+library(lattice)
+library(RColorBrewer)
+# library(tsne)
+#
+gs_expenditure_fraction = 0.35
+gs_failure_fraction = 0.9
 #
 # functions ---------------------------------------------------------------
 
@@ -63,46 +71,96 @@ result_df$behavior_sq_investment = find_sq_investment(result_df$value)
 result_df$behavior_centralized = find_centralized(result_df$value)
 result_df$kpi_failure_count = find_failure_count(result_df$value)
 result_df = subset(result_df, select = -value)
-write.csv(result_df, "edited_results_final_only.csv", row.names = F)
+# write.csv(result_df, "edited_results_final_1000.csv", row.names = F)
 
 result_df$behavior_price_tendency = factor(result_df$behavior_price_tendency, levels(result_df$behavior_price_tendency)[c(4,5,6,1,2,3)])
+df_tsne = result_df[result_df$technology.increase == 0.5,]
 
-# PLOTING -----------------------------------------------------------------
+# SUCCESS RATE OF DIFFERENT SOLUTIONS -------------------------------------
 
-par(mfrow=c(1,2))
-plot(temp$behavior_importance_tendency,temp$kpi_expenditure)
+worst_case_df = result_df[result_df$technology.increase == 0.5,]
 
-ggplot(result_df, aes(x=behavior_importance_tendency, y=kpi_expenditure, color = (technology.increase))) + geom_point() +
-  geom_jitter(width = 0.3, height = 0)
+worst_case_df$s = "BS"
+worst_case_df$s[worst_case_df$kpi_expenditure <= gs_expenditure_fraction * max(worst_case_df$kpi_expenditure) &
+                  worst_case_df$kpi_failure_count < gs_failure_fraction * max(worst_case_df$kpi_failure_count)] = "GS"
 
+length(worst_case_df$s[worst_case_df$s=="GS"])
 
-ggplot(result_df, aes(x=(behavior_importance_tendency), y=kpi_expenditure, fill = (technology.increase))) + geom_violin()
-ggplot(result_df, aes(x=(behavior_importance_tendency), y=kpi_expenditure, fill = (technology.increase))) +  geom_boxplot()
+output_space = data.frame(table(worst_case_df[5:11]))
+output_space = subset(output_space, !(behavior_importance_tendency == 1 & behavior_knowledge_tendency == 1)&
+                        !(behavior_importance_tendency == 0.5 & behavior_knowledge_tendency == 0.5) & 
+                        !(behavior_importance_tendency == 0.75 & behavior_knowledge_tendency == 1) &
+                        !(behavior_importance_tendency == 1 & behavior_knowledge_tendency == 0.75) &
+                        !(behavior_importance_tendency == 0.5 & behavior_knowledge_tendency == 0.75) &
+                        !(behavior_importance_tendency == 0.75 & behavior_knowledge_tendency == 0.5) &
+                        Freq > 0)
 
+length(output_space$s[output_space$s=="BS"])
+length(output_space$s[output_space$s=="GS"])
 
-ggplot(result_df, aes(x=(behavior_target_tendency), y=kpi_expenditure, fill = (technology.increase))) + geom_violin()
+output_space_gs = output_space[duplicated(output_space[1:6]),]
+output_space_bs = output_space[!duplicated(output_space[1:6]),]
 
-
-
-ggplot(result_df, aes(x=(behavior_price_tendency), y=kpi_expenditure, fill = (technology.increase))) + geom_violin()
-
-# etc ---------------------------------------------------------------------
-
-result_df[result_df$run.number==7,]
-
-unique(c(result_df$month.before.technology.increase, result_df$technology.increase))
-
-
-length(result_df$value)
-
-
-temp = result_df[1:100,]
-
-
-
+output_space = merge(output_space_bs, output_space_gs, all.x = T, all.y = T,by = colnames(output_space)[1:6])
+output_space$robustness.ratio = output_space$Freq.y / (output_space$Freq.x + output_space$Freq.y)
+output_space$robustness[output_space$robustness.ratio >= 0.5] = "Robust"
+output_space$robustness[output_space$robustness.ratio < 0.5] = "Un-robust"
+output_space$robustness[is.na(output_space$robustness.ratio)] = "Un-robust"
 
 
-plot_ly(data = result_df, y = ~kpi_expenditure, x = ~behavior_importance_tendency, type = "scatter", mode = "markers")
+length(output_space$robustness[output_space$robustness=="Robust"])
+
+analyzed_result_df = merge(result_df, output_space, all.x = T, all.y = F,by = colnames(output_space)[1:6])
 
 
-plot_ly(data = result_df, y = ~kpi_expenditure, x = ~behavior_importance_tendency, type = "violin")
+
+# PLOTTING ----------------------------------------------------------------
+
+ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_expenditure, fill = (technology.increase))) + geom_violin() + 
+  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure))
+ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_expenditure, fill = (technology.increase))) +  geom_boxplot() +
+  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure))
+
+
+ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_failure_count, fill = (technology.increase))) + geom_violin() +
+  geom_hline(yintercept = gs_failure_fraction * 240)
+ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_failure_count, fill = (technology.increase))) +  geom_boxplot() +
+  geom_hline(yintercept = gs_failure_fraction * 240)
+
+#
+# PLOTTING GOOD SOLUTION DISTRIBUTION -------------------------------------
+
+plot_df = output_space[output_space$robustness == "Robust",]
+plot_df[,6] = as.numeric(as.logical(plot_df[,6]))
+plot_df[,5] = as.numeric(as.logical(plot_df[,5]))
+
+for (i in 1:4) {
+  plot_df[,i] = as.numeric(as.character(plot_df[,i]))
+}
+plot_df$colorval = 1:nrow(plot_df)
+
+options(viewer=NULL)
+
+
+plot_ly(data = plot_df, type = 'parcoords',  line = list(color = ~colorval,colorscale="Viridis"),
+          dimensions = list(
+            list(range = c(0.5,1), label = 'Importance', values = ~behavior_importance_tendency, tickvals = c(0.5,0.75,1), ticktext = c("0.5","0.75","1")),
+            list(range = c(0.5,1), label = 'Knowledge', values = ~behavior_knowledge_tendency, tickvals = c(0.5,0.75,1), ticktext = c("0.5","0.75","1")),
+            list(range = c(3,18), label = 'Price', values = ~behavior_price_tendency, tickvals = c(3,6,9,12,15,18)),
+            list(range = c(0.25,1), label = 'Target', values = ~behavior_target_tendency, tickvals = c(0.25,0.5,0.75,1), 
+                 ticktext = c("0.25", "0.5","0.75","1")),
+            list(range = c(0,1), label = 'Status quo', values = ~behavior_sq_investment, ticktext = c("True", "False"), tickvals = c(1,0)),
+            list(range = c(0,1), label = 'Centralized?', values = ~behavior_centralized, ticktext = c("True", "False"), tickvals = c(1,0))
+          )
+  )
+
+
+
+
+
+# CORRELATION EXPLORATION -------------------------------------------------
+
+exp_plot = analyzed_result_df[seq(1,nrow(analyzed_result_df),75),]
+ggplot(exp_plot, aes(y=kpi_expenditure, x=kpi_failure_count, color = robustness)) + geom_point() + geom_vline(xintercept = gs_failure_fraction * 240) + 
+  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure)) + facet_grid(. ~ technology.increase)
+#
