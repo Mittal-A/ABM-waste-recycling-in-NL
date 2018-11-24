cb161355cbba44571e915b336d1db708c0839ffa
Mohammadhosein Hamedtavasoli <mhoseinhamed@gmail.com>
2018-11-24 23:20:41 +0100

    most recent R code

--- Data/Analysis.R
+++ Data/Analysis.R
@@ -1,15 +1,7 @@
 library(reshape2)
 library(ggplot2)
 library(plotly)
-# library(sdtoolkit)
-# library(prim)
-library(lattice)
-library(RColorBrewer)
-# library(tsne)
-#
-gs_expenditure_fraction = 0.35
-gs_failure_fraction = 0.9
-#
+
 # functions ---------------------------------------------------------------
 
 find_expenditure = function(value_string){
@@ -74,8 +66,31 @@ result_df = subset(result_df, select = -value)
 # write.csv(result_df, "edited_results_final_1000.csv", row.names = F)
 
 result_df$behavior_price_tendency = factor(result_df$behavior_price_tendency, levels(result_df$behavior_price_tendency)[c(4,5,6,1,2,3)])
-df_tsne = result_df[result_df$technology.increase == 0.5,]
 
+
+# GENERAL EXPLORATION -----------------------------------------------------
+
+exp_plot = result_df[seq(1,nrow(result_df),90),]
+ggplot(exp_plot, aes(y=kpi_expenditure, x=kpi_failure_count)) + geom_point(color='steelblue2') + facet_grid(. ~ technology.increase) + 
+  labs( x = "Failure count (KPI)", y = "Expenditure (KPI)")
+
+ggplot(result_df, aes(x=(technology.increase), y=kpi_expenditure, fill = (technology.increase))) + geom_violin() + 
+  labs(fill = "Technology Increase", x = "Technology increase", y = "Expenditure (KPI)")
+ggplot(result_df, aes(x=(technology.increase), y=kpi_expenditure, fill = (technology.increase))) +  geom_boxplot() +
+  labs(fill = "Technology Increase", x = "Technology increase", y = "Expenditure (KPI)")
+
+
+ggplot(result_df, aes(x = technology.increase, y=kpi_failure_count, fill = (technology.increase))) + geom_violin() + 
+  labs(fill = "Technology Increase", y = "Failure count (KPI)", x = "Technology increase")
+ggplot(result_df, aes(x=(technology.increase), y=kpi_failure_count, fill = (technology.increase))) +  geom_boxplot() +
+  labs(fill = "Technology Increase", y = "Failure count (KPI)", x = "Technology increase")
+
+#
+
+# SOLUTION ACCEPTANCE LIMIT -----------------------------------------------
+gs_expenditure_fraction = 0.85
+gs_failure_fraction = 0.85
+#
 # SUCCESS RATE OF DIFFERENT SOLUTIONS -------------------------------------
 
 worst_case_df = result_df[result_df$technology.increase == 0.5,]
@@ -117,15 +132,17 @@ analyzed_result_df = merge(result_df, output_space, all.x = T, all.y = F,by = co
 # PLOTTING ----------------------------------------------------------------
 
 ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_expenditure, fill = (technology.increase))) + geom_violin() + 
-  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure))
+  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure)) + 
+  labs(fill = "Technology Increase", x = "Robustness", y = "Expenditure (KPI)")
 ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_expenditure, fill = (technology.increase))) +  geom_boxplot() +
-  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure))
+  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure)) +
+  labs(fill = "Technology Increase", x = "Robustness", y = "Expenditure (KPI)")
 
 
 ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_failure_count, fill = (technology.increase))) + geom_violin() +
-  geom_hline(yintercept = gs_failure_fraction * 240)
+  geom_hline(yintercept = gs_failure_fraction * 240) + labs(fill = "Technology Increase", x = "Robustness", y = "Failure count (KPI)")
 ggplot(analyzed_result_df, aes(x=(robustness), y=kpi_failure_count, fill = (technology.increase))) +  geom_boxplot() +
-  geom_hline(yintercept = gs_failure_fraction * 240)
+  geom_hline(yintercept = gs_failure_fraction * 240) + labs(fill = "Technology Increase", x = "Robustness", y = "Failure count (KPI)")
 
 #
 # PLOTTING GOOD SOLUTION DISTRIBUTION -------------------------------------
@@ -152,15 +169,21 @@ plot_ly(data = plot_df, type = 'parcoords',  line = list(color = ~colorval,color
             list(range = c(0,1), label = 'Status quo', values = ~behavior_sq_investment, ticktext = c("True", "False"), tickvals = c(1,0)),
             list(range = c(0,1), label = 'Centralized?', values = ~behavior_centralized, ticktext = c("True", "False"), tickvals = c(1,0))
           )
-  )
+  )%>%
+  layout(margin = list(r = 50), font = list(size = 18))
 
 
 
 
 
-# CORRELATION EXPLORATION -------------------------------------------------
 
-exp_plot = analyzed_result_df[seq(1,nrow(analyzed_result_df),75),]
+# EXPLORATION INCLUDING ROBUSTNESS ----------------------------------------
+
+non_robust_result = analyzed_result_df[analyzed_result_df$robustness=="Un-robust",]
+robust_results = analyzed_result_df[analyzed_result_df$robustness=="Robust",]
+exp_plot = rbind(non_robust_result[seq(1,nrow(non_robust_result),90),], robust_results[seq(1,nrow(robust_results),5),])
 ggplot(exp_plot, aes(y=kpi_expenditure, x=kpi_failure_count, color = robustness)) + geom_point() + geom_vline(xintercept = gs_failure_fraction * 240) + 
-  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure)) + facet_grid(. ~ technology.increase)
+  geom_hline(yintercept = gs_expenditure_fraction * max(result_df$kpi_expenditure)) + facet_grid(. ~ technology.increase) + 
+  labs(x = "Failure count (KPI)", y = "Expenditure (KPI)", color = "Robustness")
 #
+analyzed_result_df[seq(1,nrow(analyzed_result_df),90),]
